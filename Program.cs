57bd6eb0e66740e627306025e34f7ed7d9bb7ad3
Ayomatics18 <lukmanabdulmalik80@gmail.com>
2022-06-18 11:48:56 +0100

    Assesment 1

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          /*Console.WriteLine("Enter firstName:");
+          string firstName = Console.ReadLine();
+          Console.WriteLine("firstName is:" + firstName);
+          Console.WriteLine("Enter lastName:");
+          string lastName = Console.ReadLine();
+          Console.WriteLine("lastName is:" + lastName);
+          Console.WriteLine("Enter age");
+          int age = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter favorite color:");
+          string favoritecolor = Console.ReadLine();
+          Console.WriteLine("favorite color is:" + favoritecolor);
+          Console.WriteLine("Enter favorite food:");
+          string favoritefood = Console.ReadLine();
+          Console.WriteLine("favorite food is:" + favoritefood);
+          Console.WriteLine("Enter state of origin:");
+          string stateoforigin = Console.ReadLine();
+          Console.WriteLine("state of origin is:" + stateoforigin);
+          
+
+          Console.Write("My name is " + firstName + " " + lastName +" from " + stateoforigin);
+          Console.Write(". I am " + age + " years old. ");
+          Console.WriteLine(" My favorite food is " + favoritefood + " and my favorite color is " + favoritecolor);
+
+
+          Console.Write($"My name is {firstName} {lastName}  from  {stateoforigin}. \n  I am {age} years old. \n My favorite food is {favoritefood}  and my favorite color is {favoritecolor} ");*/
+          
+          /*int age = 10;
+          Console.WriteLine(age==18);
+          Console.WriteLine(age<18);
+          Console.WriteLine(age<=18);
+          Console.WriteLine("input age :");*/
+          int age = Convert.ToInt32(Console.ReadLine());
+        
+          if (age > 18)
+          {
+              Console.WriteLine("You are eligible to vote");
+            
+          }
+          /*int r = (150 - 2) / 2 + 5;
+
+          double perimeter = 2 * Math.PI * r;
+          Console.WriteLine(perimeter);*/
+
+          /*Console.WriteLine("Enter a");
+          int a = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter b");
+          int b = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter h");
+          int h = Convert.ToInt32(Console.ReadLine());
+
+          double S = (a + b) * h / 2;
+          Console.WriteLine(S);
+
+          Console.WriteLine("The area of the trapezoid surface is " + S);*/
+
+
+
+
+          /*Console.WriteLine("Enter a");
+          int a = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter b");
+          int b = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter c");
+          int c = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Enter d");
+          int d = Convert.ToInt32(Console.ReadLine());
+
+          Console.WriteLine(a + b + c + d);
+          Console.WriteLine(${dcba});
+          Console.WriteLine(${dabc});
+          Console.WriteLine(${acbd});*/
+
+          
+
+            
+         }
+    }
+}
